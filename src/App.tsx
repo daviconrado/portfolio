476f211c1cbb5b04d3d883e97ae3470f8d94fa5b
Davi Conrado <daviconradoneto@gmail.com>
2025-09-11 14:22:32 -0300

    feat: scroll indicator

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,22 @@ import { motion, useScroll } from "motion/react";
 import SectionLine from "./components/SectionLine";
 
 function App() {
+  const { scrollYProgress } = useScroll();
   return (
     <div className="bg_dark">
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 5,
+          originX: 0,
+          backgroundColor: "#7b4ae2",
+        }}
+      />
       <Navbar />
       <main className="max-w-6xl mx-auto">
         <section id="home">
